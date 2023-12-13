afa3aa44b40a9e5f4d0eed9ff68bdcce075d00cb
Thoti Ajith <thotiajithkumar98@gmail.com>
2023-12-13 19:30:45 +0530

    add to biiitup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 
 function App() {
-  return (
+ gitreturn (
     <div className="App">
    <h1>Hello Ajith Kumar</h1>
+<<<<<<< HEAD
    <p>Git added</p>
   <p>After git added testing</p>
   <p>a sample programg b ghntn  nm</p>
+=======
+   <p>Git added tutorial</p>
+<p>After git added testing tutorials</p>
+>>>>>>> 3cb04e5cb59fa7963e0ea71d966db1d555b2ff77
     </div>
   );
 }
