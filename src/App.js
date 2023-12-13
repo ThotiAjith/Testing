3cb04e5cb59fa7963e0ea71d966db1d555b2ff77
Thoti Ajith <thotiajithkumar98@gmail.com>
2023-12-13 11:20:35 +0530

    adding the pushing details

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
  gitreturn (
     <div className="App">
    <h1>Hello Ajith Kumar</h1>
-   <p>Git added</p>
-<p>After git added testing</p>
+   <p>Git added tutorial</p>
+<p>After git added testing tutorials</p>
     </div>
   );
 }
