719f0842ab60a68cffc3beeecaeabd50251b5019
Thoti Ajith <thotiajithkumar98@gmail.com>
2023-12-13 18:19:21 +0530

    adding the paragraph

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
    <h1>Hello Ajith Kumar</h1>
    <p>Git added</p>
   <p>After git added testing</p>
-  <p>a sample programg</p>
+  <p>a sample programg b ghntn  nm</p>
     </div>
   );
 }
